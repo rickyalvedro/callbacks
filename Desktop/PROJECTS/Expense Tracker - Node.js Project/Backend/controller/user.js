ba08a848f1819bfee60120ecb66f52c504864c6a
Siraj <pintusiraj786@gmail.com>
2023-01-21 22:49:07 +0530

    Made the backend api of the login

--- Desktop/PROJECTS/Expense Tracker - Node.js Project/Backend/controller/user.js
+++ Desktop/PROJECTS/Expense Tracker - Node.js Project/Backend/controller/user.js
@@ -26,3 +26,38 @@ const signup = async (req, res) => {
     res.status(500).json(err);
   }
 };
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (isstringinvalid(email) || isstringinvalid(password)) {
+      return res
+        .status(400)
+        .json({ message: "Email id or password is missing", success: false });
+    }
+    console.log(password);
+    const user = await User.findAll({ where: { email } });
+    if (user.length > 0) {
+      if (user[0].password === password) {
+        res
+          .status(200)
+          .json({ success: true, message: "User logged in successfully" });
+      } else {
+        return res
+          .status(400)
+          .json({ success: false, message: "Password is incorrect" });
+      }
+    } else {
+      return res
+        .status(404)
+        .json({ success: false, message: "User doesnot exist" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err, success: false });
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+};
